e6c9137e7ce80ef5f6b52fb87ec630e5b3fedea7
Cody Roof <codeman2727@gmail.com>
2020-04-13 20:12:28 -0600

    got MDB working now

--- frontend/my-app/src/components/App.js
+++ frontend/my-app/src/components/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+//import logo from './logo.svg'
+import '../styles/App.css'
 import { MDBCol, MDBFormInline, MDBIcon } from 'mdbreact'
 import SearchPage from './SearchPage'
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
+      <main className='App-main'>
+        <h1><span className='roof'>Roof</span>\Cal</h1>
         <SearchPage/>
-      </header>
+      </main>
     </div>
   )
 }
--- frontend/my-app/src/components/SearchPage.js
+++ frontend/my-app/src/components/SearchPage.js
@@ -1,15 +1,17 @@
-import React from "react";
-import { MDBCol, MDBFormInline, MDBIcon } from "mdbreact";
-import 'mdbreact/dist/css/mdb.css'
-import 'font-awesome/css/font-awesome.min.css'
+import React from "react"
+import { MDBCol, MDBFormInline, MDBIcon, MDBBtn } from "mdbreact"
+import '../styles/SearchPage.css'
 
 const SearchPage = () => {
   return (
     <MDBCol md="6">
       <MDBFormInline className="md-form">
-        <MDBIcon icon="search" />
+        <MDBIcon icon="search"/>
         <input className="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search" />
       </MDBFormInline>
+      <MDBBtn outline color="pink" rounded size="sm" type="submit" className="mr-auto rounded-pill">
+        Search
+      </MDBBtn>
     </MDBCol>
   );
 }
