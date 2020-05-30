ca8074648683dc39041c7666a79a128246014851
Cody Roof <codeman2727@gmail.com>
2020-05-29 19:56:41 -0600

    did basically nothing

--- frontend/my-app/src/components/Insurance.js
+++ frontend/my-app/src/components/Insurance.js
@@ -11,33 +11,8 @@ import Checkbox from '@material-ui/core/Checkbox'
 
 const Insurance = (props) => {
   const [values, setValues] = useState({
-      zestimate: props.values.zestimates.zestimate.toLocaleString('en-US'),
-      downPayment: props.values.monthly_mortgage.down_payment.toLocaleString('en-US'),
-      downPaymentPerc: props.values.monthly_mortgage.down_payment_perc * 100,
-      loanType: '30-year',//props.values.mortgage_rate.loan_type,
-      rate: (props.values.mortgage_rate.rate * 100).toString().slice(0,5),
-      monthlyPayment: '',
-      percActive: false
+      pmi: props.values.pmi.yearly.toLocaleString('en-US'),
     })
-
-
-    const monthlyPaymentCalc = (housePrice, downPayment, loanType, rate) => {
-      let loan = housePrice.toString().replace(/,/g, '') - downPayment.toString().replace(/,/g, '')
-      let numPayments = parseInt(loanType.substring(0, 2)) * 12 // 12 Months in year
-      let monthlyRate = parseFloat((rate/100)/12) // 12 Months in year
-      let monthlyPayment = Math.round(loan*(monthlyRate*Math.pow((1 + monthlyRate), numPayments))/(Math.pow((1 + monthlyRate), numPayments) - 1)) || 0
-
-      setValues({...values, monthlyPayment: formatter.format(monthlyPayment)})
-    }
-
-    const downPaymentCalc = (type, value, housePrice) => {
-      if (type === 'percent') {
-        setValues({...values, downPayment: formatter.format(Math.round((value/100) * housePrice.replace(/,/g, ''))).replace('$', '')}) //divide by 100 to get it as a percent again
-      }
-      else {
-        setValues({...values, downPaymentPerc: (value.replace(/,/g, '')/housePrice.replace(/,/g, '') * 100).toFixed(1)})
-      }
-    }
 
     const handleChange = (event, value) => {
       const re = /^[.,0-9\b]+$/;
@@ -51,28 +26,6 @@ const Insurance = (props) => {
       }
     }
 
-  useEffect(() => {
-    monthlyPaymentCalc(values.zestimate, values.downPayment, values.loanType, values.rate)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [values.zestimate, values.downPayment, values.downPaymentPerc, values.loanType, values.rate]);
-
-  useEffect(() => {
-    if (values.percActive) return //Don't want current textfield changing while the user is changing it
-    downPaymentCalc('amount', values.downPayment, values.zestimate)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [values.downPayment]);
-
-  useEffect(() => {
-    if (!values.percActive) return //Don't want current textfield changing while the user is changing it
-    downPaymentCalc('percent', values.downPaymentPerc, values.zestimate)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [values.downPaymentPerc]);
-
-  useEffect(() => {
-   monthlyPaymentCalc(values.zestimate, values.downPayment, values.loanType, values.rate)
-   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
@@ -81,6 +34,7 @@ const Insurance = (props) => {
 
   return (
     <ExpansionPanel>
+    {console.log(props)}
       <ExpansionPanelSummary
       expandIcon={<ExpandMoreIcon className='expand-icon'/>}
       aria-controls='panel1a-content'
@@ -92,14 +46,16 @@ const Insurance = (props) => {
       </ExpansionPanelSummary>
       <ExpansionPanelDetails>
         <FormControl fullWidth>
+        <div className='two-column'>
         <Checkbox
+          label='You usually pay Mortgage Insurance if your down payment is less than 20%'
           color='primary'
         />
         <TextField
             label='Mortgage Insurance'
             variant='filled'
             name='zestimate'
-            value={values.zestimate}
+            value={values.pmi}
             InputProps={{
                 startAdornment: (
                   <InputAdornment position="start">
@@ -107,8 +63,11 @@ const Insurance = (props) => {
                   </InputAdornment>
                 ),
               }}
+            labelPlacement='end'
+            inputProps={{ style: {textAlign: 'right'} }}
             onChange={(event, value)=> handleChange(event, value)}
           />
+          </div>
         </FormControl>
       </ExpansionPanelDetails>
     </ExpansionPanel>
